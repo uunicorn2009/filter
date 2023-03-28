261e63a9d0b5387532419e9bec2b326b6a9d2b30
papa'sfariy <77390644+uunicorn2009@users.noreply.github.com>
2023-03-28 10:09:58 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,20 +1,10 @@
+noseX=0;
+noseY=0;
 function preload (){
-
-}
-function setup(){
-canvas=createCanvas(300,300);
-canvas.center();
-}
-function draw(){
-
-}
-function take_snapshot(){
-save("MyFirstImage.png"); 
+mustache_img=loadImage("https://i.postimg.cc/ydjv4n7F/images-1-removebg-preview-2.png");
 }
 
-function preload (){
 
-}
 function setup(){
 canvas=createCanvas(300,300);
 canvas.center();
@@ -26,6 +16,7 @@ poseNet.on ('pose',gotPoses);
 }
 function draw(){
 image(video,0,0,300,300);
+image(mustache_img,noseX-15,noseY+15,30,30);
 
 }
 function take_snapshot(){
@@ -38,7 +29,9 @@ function modelLoaded(){
 function gotPoses(results){
     if(results.length > 0){
         console.log(results);
-        console.log("nose X = "+results[0].pose.nose.x);
-        console.log("nose Y = "+results[0].pose.nose.y);
+        noseX=results[0].pose.nose.x;
+        noseY=results[0].pose.nose.y;
+        console.log("nose X = "+noseX);
+        console.log("nose Y = "+noseY);
     }
 }
